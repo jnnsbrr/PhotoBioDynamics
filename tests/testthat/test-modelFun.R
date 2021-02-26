60623b5e72e80504b1bdb6e858ac0281faa24e35
Jannes <jbreier@gfz-potsdam.de>
2021-02-26 21:36:57 +0100

    test debug

--- tests/testthat/test-modelFun.R
+++ tests/testthat/test-modelFun.R
@@ -1,8 +1,9 @@
 # test modelFun
 test_that(".modelFun", {
 
-  temp = readRDS(file="../testdata/temp_ras.Rds") %>% 
-    raster::as.array(.)
+  temp_ras = readRDS(file="../testdata/temp_ras.Rds")
+  temp = raster::as.array(temp_ras)
+  
   fpar = readRDS(file="../testdata/fpar_ras.Rds") %>% 
     raster::as.array(.)
   lai = readRDS(file="../testdata/lai_ras.Rds") %>% 
@@ -20,12 +21,11 @@ test_that(".modelFun", {
                                      co2= co2,
                                      paramfile = "../../inst/data/input/model_parameters.YAML")
 
-  testthat::expect_equal(dim(test_gpp),c(360,720,1))
+  testthat::expect_equal(dim(test_gpp),c(360,720))
   
-  test_value = cellStats(x = test_gpp * area(test_gpp),
-                         stat = sum, 
-                         na.rm = TRUE
-                         )*1e-9
+  test_gpp = array(test_gpp,c(dim(test_gpp),1))
+  test_value = sum(test_gpp * raster::as.array(area(temp_ras)), 
+                   na.rm = TRUE)*1e-9
   
   testthat::expect_gt(test_value, 5)
 })
